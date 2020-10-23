b20ff04d4bb94ea524bea0b10200d5b30f992945
rolara57 <roger@qm46.com>
2020-10-23 15:25:56 -0500

    changed port to 3000 from 8080

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT ||3000;
 
 // Requiring our models for syncing
 var db = require("./models");
