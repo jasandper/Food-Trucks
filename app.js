8b2e6d1f9b4c4af726a6983c73636bd5a8754b1b
Jason Perez <jasandper@icloud.com>
2020-10-26 22:03:45 -0500

    verify connection to datbase

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const Sequelize = require('sequelize');
+const db = new Sequelize('foodtrucks', 'postgres', '5276', {
+    host: 'localhost',
+    dialect: 'postgres',
+    
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+});
+
+// Test DB
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err))
+
 const app = express();
 
 app.get('/', (req, res) => res.send('INDEX'));
