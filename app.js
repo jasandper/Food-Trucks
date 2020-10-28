d7f3110162f3d2f1d65642844d6b7c882f2de0b3
Jason Perez <jasandper@icloud.com>
2020-10-27 22:22:16 -0500

    add index route to app.js

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.set('view engine', 'handlebars');
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.send('INDEX'));
+// Index route
+app.get('/', (req, res) => res.render('index', { layout: 'landing'}));
 
 // Truck routes
 app.use('/trucks', require('./routes/trucks'));
@@ -35,5 +36,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
-// teset 
+
 
