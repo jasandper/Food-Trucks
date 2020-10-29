25c0e4cb9bfd6cd64fc5871a8304398295c475c3
rolara57 <roger@qm46.com>
2020-10-29 18:50:20 -0500

    added read me

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Bthootu16",
-  database: "parties_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-module.exports = connection;
 // *****************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
 //
