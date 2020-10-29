d1210b588aa2e84e3bb6acc38fc02bddb33f7399
Jason Perez <jasandper@icloud.com>
2020-10-29 18:00:54 -0500

    update db

--- config/database.js
+++ config/database.js
@@ -1,14 +1,20 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('foodtrucks', 'postgres', '5276', {
-    host: 'localhost',
-    dialect: 'postgres',
-    
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-});
+if(process.env.DATABASE_URL) {
+    module.exports = new Sequelize(process.env.DATABASE_URL)
+} else {
+    module.exports = new Sequelize('foodtrucks', 'postgres', '5276', {
+        host: 'localhost',
+        dialect: 'postgres',
+        
+    
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+    });
+}
+
